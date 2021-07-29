7e7de0f5a7e6dee2a3da18504f4e2b133d0d419f
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-07-29 08:06:25 +0000

    Add images to workspace and test the event listeners is JS

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+        
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            console.log('welldone');
+            alert("You clicked a button!");
+        })
+    }
+})
