0aba5306216a35bb2b573fac349f3c75f9038b52
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-08-02 11:08:36 +0000

    Add correct button functionality for difficulties

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,73 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");    
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("id") === "reset-game") {
-                resetGame();
-            } else if (this.getAttribute("class") === "diff-btn") {
-                changeDifficulty();
-            } else {
-                alert("Error! Button function not known.");
-            }
-        })
-    }
-    startGame();
-})
+document.addEventListener("DOMContentLoaded", startGame);
+
 
 function startGame() {
     addTime();
+}
+
+const diffButtons = document.getElementsByClassName("diff-btn");
+
+for (let i = 0; i < diffButtons.length; i++) {
+    diffButtons[i].addEventListener("click", changeDifficulty); // Add an onclick to select the level
+}
+
+const easyClass = document.getElementsByClassName("easy");
+const mediumClass = document.getElementsByClassName("medium");
+const hardClass = document.getElementsByClassName("hard");
+const hardLevel = [...mediumClass, ...hardClass, ...easyClass];
+let deck = document.getElementById("deck");
+
+function showEasy(){
+    for (let i = 0; i < mediumClass.length; i++){
+        let mediumLi = mediumClass[i];
+        mediumLi.style.display = "none";
+    }
+    for (let i = 0; i < hardClass.length; i++){
+        let hardLi = hardClass[i];   
+        hardLi.style.display = "none" 
+    }
+    deck.style.width = "500px";
+}
+
+function showMedium(){
+    for (let i = 0; i < hardClass.length; i++){
+        let hardLi = hardClass[i];   
+        hardLi.style.display = "none"
+    }
+    for (let i = 0; i < mediumClass.length; i++){
+        let mediumLi = mediumClass[i];
+        mediumLi.style.display = "flex";
+    }
+    deck.style.width = "600px"
+}
+
+function showHard(){
+    for (let i = 0; i < hardLevel.length; i++){
+        let toughLi = hardLevel[i];
+        toughLi.style.display = "flex";
+    }
+    deck.style.width = "760px";
+}
+
+
+function changeDifficulty() {
+   level = this.id;
+
+   if (level == "diff-easy"){
+        showEasy();
+   } else if (level == "diff-medium"){
+       showMedium();
+   } else if (level == "diff-hard"){
+       showHard();
+   }
+}
+
+const resetBtn = document.getElementById("reset-game").addEventListener('click', resetGame);
+
+function resetGame() {
+    resetTimer();
+    console.log("reset");
 }
 
 function mixCards() {}
@@ -27,33 +78,10 @@ function increaseScore() {}
 
 function changeScoreColor() {}
 
-function changeDifficulty() {
-    let buttons = document.getElementsByClassName("diff-btn");
-    let diffButton = "";
-
-    for (let button of buttons) {
-        let diffButton = button.id;
-
-        if (diffButton == "diff-easy"){
-            console.log("easy button");
-            
-        } else if (diffButton == "diff-medium"){
-            console.log("medium button");
-            
-        } else if (diffButton == "diff-hard"){
-            console.log("hard button");
-            
-        }
-    }
-}
-
-
-function resetGame() {
-    resetTimer();
-    console.log("reset");
-}
 
 function congratsMessage() {}
+
+
 
 /* Timer variables */
 var timerSpan = document.getElementById("timer"),
@@ -70,7 +98,7 @@ function addTime() {
         minutes++;
         }
     
-    timerSpan.textContent = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + " Minutes " + (seconds > 9 ? seconds : "0" + seconds) + " Seconds";
+    timerSpan.innerHTML = (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + " Minutes " + (seconds > 9 ? seconds : "0" + seconds) + " Seconds";
     timer();
 }
 
